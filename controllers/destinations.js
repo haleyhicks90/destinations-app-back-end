fb61a1117de939d7ab16b7bf492cc23af403fb06
Haley Hicks <haleyhicks90@yahoo.com>
2021-08-07 15:12:34 -0400

    added seed route

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -31,6 +31,37 @@ router.put('/:id', (req, res) => {
     })
 })
 
+//seed route (GET)
+// post.get ('/sdjfsjdkfjsdkfdks/seed', (req, res) => {
+//     Post.deleteMany({}, () => {})
+
+router.get('/seed', (req, res) => {
+    Destinations.create(
+        [
+            {
+                location: "Italy",
+                image: "https://thepointsguy.global.ssl.fastly.net/uk/originals/2020/10/GettyImages-1170100071.jpg",
+                language: "Italian",
+                population: 60.36
+            },
+            {
+                location: "Rio de Janeiro, Brazil",
+                image: "https://gsg-61ea.kxcdn.com/media/contents/Rio%20city%20and%20rocks%20lucas-campoi-pHnzdSAEkRM-unsplash.jpg",
+                language: "Portuguese",
+                population: 6.748
+            },
+            {
+                location: "New Zealand",
+                image: "https://deih43ym53wif.cloudfront.net/travel-to-new-zealand-rough-guides_1436a026cc.jpeg",
+                language: "English, Te Reo Māori",
+                population: 4.917
+            }
+        ],
+        (error, data) => {
+            res.redirect('/')
+        }
+    )
+})
 
 
 //exporting
