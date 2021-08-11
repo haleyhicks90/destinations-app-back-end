b8103962fba25fdc59e9e87e5a2e1ca6100de1ca
Haley Hicks <haleyhicks90@yahoo.com>
2021-08-10 20:01:01 -0400

    testing heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
+const db = mongoose.connection;
 
 // =============== Controllers =============== //
 const destinationsController = require('./controllers/destinations.js')
@@ -15,9 +16,12 @@ require('dotenv').config()
 app.use('/destinations', destinationsController)
 // app.use('/', userController)
 
+db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
+db.on("connected", () => console.log("mongo connected: ", MONGODB_URI));
+db.on("disconnected", () => console.log("mongo disconnected "));
 
 // =============== Database =============== //
-const PORT = process.env.port || 3003
+const PORT = process.env.PORT || 3000
 const MONGODB_URI = process.env.MONGODB_URI;
 mongoose.connect(MONGODB_URI ,
     {
